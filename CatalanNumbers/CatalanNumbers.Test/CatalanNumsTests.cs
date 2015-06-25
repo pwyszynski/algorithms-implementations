48dadb3bb7f1f4048600ba89f005164c0f399f11
Przemyslaw Wyszynski <stuntmankg@gmail.com>
2015-06-25 22:20:53 +0200

    Catalan Numbers (direct approach) with tests

---
+++ CatalanNumbers/CatalanNumbers.Test/CatalanNumsTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CatalanNumbers.Methods;
+
+namespace CatalanNumbers.Test
+{
+	[TestClass]
+	public class CatalanNumsTests
+	{
+		[TestMethod]
+		public void CatalanNumberOf5Is42()
+		{
+			Assert.AreEqual(42, CNums.CatalanDirect(5));
+		}
+	}
+}
