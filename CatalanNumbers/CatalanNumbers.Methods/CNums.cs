67d664fae7fd451a7f3b37389a9274686707b3f5
Przemyslaw Wyszynski <stuntmankg@gmail.com>
2015-06-25 22:24:48 +0200

    Whoopsies

---
+++ CatalanNumbers/CatalanNumbers.Methods/CNums.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalanNumbers.Methods
+{
+    public class CNums
+    {
+		public static int Factorial(int n)
+		{
+			int result = 0;
+			if (n > 1)
+			{
+				result = n * Factorial(n - 1);
+			}
+			else if (n == 0 || n == 1)
+			{
+				return 1;
+			}
+			else
+			{
+				throw new System.ArithmeticException();
+			}
+			return result;
+		}
+
+		public static int CatalanDirect(int n)
+		{
+			var result = Factorial(2 * n) / (Factorial(n + 1) * Factorial(n));
+			return result;
+		}
+    }
+}
---
+++ CatalanNumbers/CatalanNumbers.Test/FactorialTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CatalanNumbers.Methods;
+
+namespace CatalanNumbers.Test
+{
+	[TestClass]
+	public class FactorialTests
+	{
+		[TestMethod]
+		public void FactorialFrom1Is1()
+		{
+			Assert.AreEqual(1, CNums.Factorial(1));
+		}
+
+		[TestMethod]
+		public void FactorialFrom0Is1()
+		{
+			Assert.AreEqual(1, CNums.Factorial(0));
+		}
+
+		[TestMethod]
+		public void FactorialFrom5Is120()
+		{
+			Assert.AreEqual(120, CNums.Factorial(5));
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArithmeticException))]
+		public void FactorialFromMinus1ThrowsException()
+		{
+			CNums.Factorial(-2);
+		}
+	}
+}
